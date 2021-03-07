d9535e5781434ced135850c1ef6f5382a81a30e5
braylin16 <ibanpallano1997@gmail.com>
2021-03-07 17:40:11 -0400

    Agregamos Smojis al chat

---
+++ emoticos/emoticos.php
@@ -0,0 +1,33 @@
+<!-- Dropdown Structure -->
+<ul id='emotico' class='dropdown-content'>
+    <li><span id="s1">😀</span></li>
+    <li><span id="s2">🤣</span></li>
+    <li><span id="s3">😍</span></li>
+    <li><span id="s4">😘</span></li>
+    <li><span id="s5">😜</span></li>
+    <li><span id="s6">😎</span></li>
+    <li><span id="s7">😇</span></li>
+    <li><span id="s8">🤨</span></li>
+    <li><span id="s9">😷</span></li>
+    <li><span id="s10">🤬</span></li>
+    <li><span id="s11">😈</span></li>
+    <li><span id="s12">🤡</span></li>
+    <li><span id="s13">💩</span></li>
+    <li><span id="s14">🤑</span></li>
+    <li><span id="s15">😱</span></li>
+    <li><span id="s16">😭</span></li>
+    <li><span id="s17">😢</span></li>
+    <li><span id="s18">🤠</span></li>
+    <li><span id="s19">🎃</span></li>
+    <li><span id="s20">👍</span></li>
+    <li><span id="s21">👎</span></li>
+    <li><span id="s22">🤘</span></li>
+    <li><span id="s23">🖕</span></li>
+    <li><span id="s24">👋</span></li>
+    <li><span id="s25">🙏</span></li>
+    <li><span id="s26">👀</span></li>
+    <li><span id="s27">👮</span></li>
+    <li><span id="s28">👮‍♀️</span></li>
+    <li><span id="s29">👨‍💻</span></li>
+    <li><span id="s30">🕵️‍♂️</span></li>
+</ul>
